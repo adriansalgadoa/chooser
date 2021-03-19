c7be5b61cba65069a3c6e192d7f86460c7c3d58a
Adrian Salgado <adriansalgado@Adrians-MacBook-Pro.local>
2021-03-19 16:36:41 +0100

    Added git interactions, get branch list, delete if -D is passed and checkout to selected branch

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const inquirer = require('inquirer');
+const exec = require('child_process').exec;
 
 const DELETE_FLAG = '-D';
 const flags = [DELETE_FLAG];
 
 const foundFlags = process.argv.find(arg => flags.find(flag => flag === arg));
 
-const prompt = [{
-	choices: ['branch 1', 'branch 2', 'branch 3'],
+const prompt = branchList => ([{
+	choices: branchList,
 	default: 'default',
 	message: 'message',
 	name: 'name',
 	type: 'list',
-}];
+}]);
 
-const processAnswers = answers => {
-	console.log('answers: ', answers);
+const processAnswers = ({ name }) => {
 	if (foundFlags === DELETE_FLAG) {
-		console.log('git branch -D ' + answers.name);
+		// Ask for confirmation
+		exec('git branch -D ' + name, () => console.log('Branch "' + name + '" has been deleted.'));
 		return;
 	}
 
-	console.log('git checkout ' + answers.name);
+	exec('git checkout ' + name);
+	console.log('Changed to branch: ' + name);
 	return;
 }
 
@@ -28,7 +30,12 @@ const catchError = error => {
 	console.log('error: ', error);
 }
 
-inquirer
-	.prompt(prompt)
-	.then(processAnswers)
-	.catch(catchError);
+exec('git branch', (err, out, _err) => {
+	const branchList = out.replace(/ /g, '')
+		.split('\n').filter(a => a);
+
+	inquirer
+		.prompt(prompt(branchList))
+		.then(processAnswers)
+		.catch(catchError);
+});
